cf5ade8d23b681fd1a236471e99df2c64ebfb374
devin <2377002196@qq.com>
2021-01-21 11:20:15 +0800

    commit

---
+++ VideoEncode/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  VideoEncode
+//
+//  Created by devin on 2021/1/21.
+//  Copyright © 2021 devin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet UIButton *startBtn;
+
+@end
+
---
+++ VideoEncodeTests/VideoEncodeTests.m
@@ -0,0 +1,37 @@
+//
+//  VideoEncodeTests.m
+//  VideoEncodeTests
+//
+//  Created by devin on 2021/1/21.
+//  Copyright © 2021 devin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VideoEncodeTests : XCTestCase
+
+@end
+
+@implementation VideoEncodeTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
